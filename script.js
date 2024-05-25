e3190e75f17217bdd22fe1a069bbdceef060161d
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-05-25 22:13:25 +0600

    Kaaj?

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
       const receiverId = 'beat_koloth.testnet';
       const deposit = '100000000000000000000'; // Amount in yoctoNEAR (1 NEAR)
       
-      const _actions = [{
+      const _actions = {
         type: "Transfer",
         params: {
           deposit: deposit
         },
-      }];
-
-      const transferTransaction = {
-        signerId,
-        receiverId,
-        actions: _actions
       };
+
+      const transferTransaction = [{
+        receiverId,
+        signerId,
+        actions: _actions
+      }];
 
       const encodedTransactionData = encodeURIComponent(encodeURI (JSON.stringify(transferTransaction)));
       const callbackUrl = encodeURIComponent(window.location.origin + window.location.pathname);
