07b1e276855f85a4566a8833f33f048a2a4027ca
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-05-26 06:32:36 +0600

    nana

--- script.js
+++ script.js
@@ -1,56 +1,18 @@
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
   const connectWalletButton = document.getElementById('connectWalletButton');
-  const signTransactionButton = document.getElementById('signTransactionButton');
-  const mainPage = document.getElementById('mainPage');
-  const successPage = document.getElementById('successPage');
-  const transactionPage = document.getElementById('transactionPage');
-  const accountIdElement = document.getElementById('accountId');
-  const publicKeyElement = document.getElementById('publicKey');
-  const transactionHashesElement = document.getElementById('transactionHashes');
+  // const signTransactionButton = document.getElementById('signTransactionButton');
+  // const mainPage = document.getElementById('mainPage');
+  // const successPage = document.getElementById('successPage');
+  // const transactionPage = document.getElementById('transactionPage');
+  // const accountIdElement = document.getElementById('accountId');
+  // const publicKeyElement = document.getElementById('publicKey');
+  // const transactionHashesElement = document.getElementById('transactionHashes');
 
   connectWalletButton.addEventListener('click', () => {
-    const successUrl = encodeURIComponent(window.location.origin + window.location.pathname);
-    const connectUrl = `https://testnet.wallet.mintbase.xyz/connect?success_url=${successUrl}`;
-    window.location.href = connectUrl;
-  });
-
-
-    signTransactionButton.addEventListener('click', () => {
-      const signerId = 'matrix-harrison.testnet';
-      const receiverId = 'beat_koloth.testnet';
-      const deposit = '100000000000000000000'; // Amount in yoctoNEAR (1 NEAR)
-      
-      const _actions = [{
-        type: "Transfer",
-        params: {
-          deposit: deposit
-        },
-      }];
-
-      const transferTransaction = [{
-        receiverId,
-        signerId,
-        actions: _actions
-      }];
-
-      const encodedTransactionData = encodeURIComponent(encodeURI (JSON.stringify(transferTransaction)));
-      const callbackUrl = encodeURIComponent(window.location.origin + window.location.pathname);
-      const signTransactionUrl = `https://testnet.wallet.mintbase.xyz/sign-transaction?transactions_data=${encodedTransactionData}&callback_url=${callbackUrl}`;
-      window.location.href = signTransactionUrl;
-  });
-
-  const urlParams = new URLSearchParams(window.location.search);
-  const accountId = urlParams.get('account_id');
-  const publicKey = urlParams.get('public_key');
-  const transactionHashes = urlParams.get('transactionHashes');
-
-  if (accountId && publicKey) {
-    mainPage.style.display = 'none';
-    successPage.style.display = 'block';
-
-    accountIdElement.textContent = `Account ID: ${accountId}`;
-    publicKeyElement.textContent = `Public Key: ${publicKey}`;
+    // const successUrl = encodeURIComponent(window.location.origin + window.location.pathname);
+    // const connectUrl = `https://testnet.wallet.mintbase.xyz/connect?success_url=${successUrl}`;
+    // window.location.href = connectUrl;
 
     if (window.vuplex) {
       // The window.vuplex object already exists, so go ahead and send the message.
@@ -60,6 +22,46 @@ document.addEventListener('DOMContentLoaded', () => {
       // loading, so add an event listener to send the message once it's initialized.
       window.addEventListener('vuplexready', sendMessageToCSharp);
   }
+  });
+
+
+  //   signTransactionButton.addEventListener('click', () => {
+  //     const signerId = 'matrix-harrison.testnet';
+  //     const receiverId = 'beat_koloth.testnet';
+  //     const deposit = '100000000000000000000'; // Amount in yoctoNEAR (1 NEAR)
+      
+  //     const _actions = [{
+  //       type: "Transfer",
+  //       params: {
+  //         deposit: deposit
+  //       },
+  //     }];
+
+  //     const transferTransaction = [{
+  //       receiverId,
+  //       signerId,
+  //       actions: _actions
+  //     }];
+
+  //     const encodedTransactionData = encodeURIComponent(encodeURI (JSON.stringify(transferTransaction)));
+  //     const callbackUrl = encodeURIComponent(window.location.origin + window.location.pathname);
+  //     const signTransactionUrl = `https://testnet.wallet.mintbase.xyz/sign-transaction?transactions_data=${encodedTransactionData}&callback_url=${callbackUrl}`;
+  //     window.location.href = signTransactionUrl;
+  // });
+
+  // const urlParams = new URLSearchParams(window.location.search);
+  // const accountId = urlParams.get('account_id');
+  // const publicKey = urlParams.get('public_key');
+  // const transactionHashes = urlParams.get('transactionHashes');
+
+  // if (accountId && publicKey) {
+  //   mainPage.style.display = 'none';
+  //   successPage.style.display = 'block';
+
+  //   accountIdElement.textContent = `Account ID: ${accountId}`;
+  //   publicKeyElement.textContent = `Public Key: ${publicKey}`;
+
+   
   
   function sendMessageToCSharp() {
       // This object passed to postMessage() automatically gets serialized as JSON
