3c53caf33d3927c6f00e77b7d11d549a2f3bfe6e
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-06-14 20:57:11 +0600

    Responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
   const connectWalletButton = document.getElementById('connectWalletButton');
   const signTransactionButton = document.getElementById('signTransactionButton');
@@ -15,35 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.href = connectUrl;
   });
 
+  signTransactionButton.addEventListener('click', () => {
+    const tg = window.Telegram.WebApp;
+    const urlParams = new URLSearchParams(window.location.search);
+    const accountId = urlParams.get('account_id');
+    const publicKey = urlParams.get('public_key');
 
-    signTransactionButton.addEventListener('click', () => {
-      // const signerId = 'matrix-harrison.testnet';
-      // const receiverId = 'beat_koloth.testnet';
-      // const deposit = '100000000000000000000'; // Amount in yoctoNEAR (1 NEAR)
-      
-      // const _actions = [{
-      //   type: "Transfer",
-      //   params: {
-      //     deposit: deposit
-      //   },
-      // }];
-
-      // const transferTransaction = [{
-      //   receiverId,
-      //   signerId,
-      //   actions: _actions
-      // }];
-
-      // const encodedTransactionData = encodeURIComponent(encodeURI (JSON.stringify(transferTransaction)));
-      // const callbackUrl = encodeURIComponent(window.location.origin + window.location.pathname);
-      // const signTransactionUrl = `https://testnet.wallet.mintbase.xyz/sign-transaction?transactions_data=${encodedTransactionData}&callback_url=${callbackUrl}`;
-      // window.location.href = signTransactionUrl;
-      const tg = window.Telegram.WebApp;
-      const urlParams = new URLSearchParams(window.location.search);
-      const accountId = urlParams.get('account_id');
-      const publicKey = urlParams.get('public_key');
-
-      tg.sendData(accountId); 
+    tg.sendData(accountId);
   });
 
   const urlParams = new URLSearchParams(window.location.search);
@@ -57,11 +34,4 @@ document.addEventListener('DOMContentLoaded', () => {
     accountIdElement.textContent = `Account ID: ${accountId}`;
     publicKeyElement.textContent = `Public Key: ${publicKey}`;
   }
-
-  // if (transactionHashes) {
-  //   successPage.style.display = 'none';
-  //   transactionPage.style.display = 'block';
-
-  //   transactionHashesElement.textContent = `Transaction Hashes: ${transactionHashes}`;
-  // }
 });
