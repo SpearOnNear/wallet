d9ab021a78faf2514a1fe1b958fa37bc3b8dd709
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2024-07-07 13:24:39 +0600

    new update

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const connectWalletButton = document.getElementById('connectWalletButton');
-  const signTransactionButton = document.getElementById('signTransactionButton');
-  const mainPage = document.getElementById('mainPage');
-  const successPage = document.getElementById('successPage');
-  const transactionPage = document.getElementById('transactionPage');
+  const signTransactionButton = document.getElementById('continuebtn');
   const accountIdElement = document.getElementById('accountId');
-  const publicKeyElement = document.getElementById('publicKey');
-  const transactionHashesElement = document.getElementById('transactionHashes');
-
-  connectWalletButton.addEventListener('click', () => {
-    const successUrl = encodeURIComponent(window.location.origin + window.location.pathname);
-    const connectUrl = `https://testnet.wallet.mintbase.xyz/connect?success_url=${successUrl}`;
-    window.location.href = connectUrl;
-  });
-
-  signTransactionButton.addEventListener('click', () => {
-    const tg = window.Telegram.WebApp;
-    const urlParams = new URLSearchParams(window.location.search);
-    const accountId = urlParams.get('account_id');
-    const publicKey = urlParams.get('public_key');
-
-    tg.sendData(accountId);
-  });
 
   const urlParams = new URLSearchParams(window.location.search);
   const accountId = urlParams.get('account_id');
-  const publicKey = urlParams.get('public_key');
 
-  if (accountId && publicKey) {
-    mainPage.style.display = 'none';
-    successPage.style.display = 'block';
-
-    accountIdElement.textContent = `Account ID: ${accountId}`;
+  if (accountId) {
+    accountIdElement.value = accountId;
   }
+
+  console.log(accountIdElement.value)
+
+  signTransactionButton.addEventListener('click', () => {
+    const tg = window.Telegram.WebApp;
+    tg.sendData(accountId);
+  });
+
+  
 });
